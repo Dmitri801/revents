a8af079818957598fbe752579bf5120a0a4ae7a8
Dmitriy Sharshiner <dmitriysharshiner@Dmitriys-MacBook-Air.local>
2018-05-24 21:04:32 -0600

    end of section 1

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import Navbar from "../../features/nav/navbar/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-Vents</h1>
+      <div>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
 }
 
-
 export default App;
-
